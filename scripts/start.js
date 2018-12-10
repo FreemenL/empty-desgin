75e2d0d3016b2380103d978fadbafb2685f246b8
xiajiang <liuxiajiang818@163.com>
2018-12-10 23:57:49 +0800

    -

--- scripts/start.js
+++ scripts/start.js
@@ -1,13 +1,13 @@
-// const { rm, cp, mkdir, exec, echo } = require('shelljs');
-// const chalk = require('chalk');
+const { rm, cp, mkdir, exec, echo } = require('shelljs');
+const chalk = require('chalk');
 
-// console.log(chalk.green('You application is start building...'));
-// // exec("cross-env NODE_ENV=development pm2 --watch ../build/* --watch ../package.json");
-// const pwd = process.cwd()
+console.log(chalk.green('You application is start building...'));
+// exec("cross-env NODE_ENV=development pm2 --watch ../build/* --watch ../package.json");
+const pwd = process.cwd()
 
-// exec(`${pwd}/node_modules/.bin/webpack --config ${pwd}/build/webpack.config.js`, function(res) {
-//     console.log(res)
-// });
+exec(`${pwd}/node_modules/.bin/webpack --config ${pwd}/build/webpack.config.js`, function(res) {
+    console.log(res)
+});
 // exec('pwd')
-const {run} = require('runjs')
-run('webpack --config ./build/webpack.config.js')
+// const {run} = require('runjs')
+// run('webpack --config ./build/webpack.config.js')
